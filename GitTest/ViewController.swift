afb49daca7a2de196c10cc8dc6789f93bd3b7641
Emre Ünal <>
2019-06-20 15:34:30 +0300

    All commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Emre Ünal on 20.06.2019.
+//  Copyright © 2019 Emre Ünal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("github")
+    }
+
+
+}
+
